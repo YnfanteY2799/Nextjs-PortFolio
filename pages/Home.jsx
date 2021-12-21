4f69d54e668ed98ff5f6cbca14f524e188d520e0
Yassett J Ynfante Jimenez <49245097+YnfanteY2799@users.noreply.github.com>
2021-12-20 23:14:17 -0400

    +Updates

--- pages/Home.jsx
+++ pages/Home.jsx
@@ -1,34 +1,50 @@
-import { AppBar } from "../components/NavigationComponents.jsx";
+import { AppBar, Hero } from "../components/NavigationComponents.jsx";
+import { useEffect, useState } from "react";
+import Image from "next/image";
+
+function HomeBody(pfp = null){
+
+  return <div className="center">
+
+    <image src={pfp}/>
+    <p>
+      {`Hi, i'm Yassett Jose Ynfante Jimenez`}
+    </p>
+
+
+  </div>;
+
+}
+
 
 
 
 
 export default function Home(){
 
+  const [ profilePicture, setProfilePicture ] = useState(null);
 
 
+  
+  useEffect(
+    ()  => {
+      fetch("https://github.com/YnfanteY2799.png")
+      .then(x => console.log(x))
+      // .then(x =>{console.log(x) ;setProfilePicture(x)})
+    }
+
+
+  )
 
   return <>
       <AppBar />
-    <div>
-
-      <div className="container">
-
-        <ul className="slider">
-          <li id="slide1">
-            a
-          </li>
-          <li id="slide2">
-            b
-          </li>
-          <li id="slide3">
-            c
-          </li>
-        </ul>
+        <div>
+          <Hero color="black" title = {HomeBody(profilePicture)} size="ism"/>
+          <Hero color="black" title = {HomeBody()} size="ism"/>
         
-      </div>
 
-    </div>
+
+        </div>
     </>;
 
 }
--- pages/api/sampleDataCaller.js
+++ pages/api/sampleDataCaller.js
@@ -2,6 +2,9 @@
 
 export default function handler(req, res) {
 
+
+
+
   res.status(200).json([
     {
       division: "NodeJs Projects ", 
