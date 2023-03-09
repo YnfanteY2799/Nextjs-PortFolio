59615f8d887c83a81f5974b73e4889f5e9b3151a
Yassett J Ynfante Jimenez <49245097+YnfanteY2799@users.noreply.github.com>
2023-03-09 03:50:03 -0400

    +added 3D Models

--- src/components/ui/Navbar/Navbar.tsx
+++ src/components/ui/Navbar/Navbar.tsx
@@ -7,7 +7,9 @@ export default function Navbar(): ReactElement {
   const [active, setActive] = useState("" as string);
   const [isToggle, setToggle] = useState(false as boolean);
 
-  function handleActive(event: MouseEvent<HTMLAnchorElement, globalThis.MouseEvent> | string): void {
+  function handleActive(
+    event: MouseEvent<HTMLAnchorElement, globalThis.MouseEvent> | string
+  ): void {
     typeof event === "string" ? setActive(event) : setActive("");
     window.scrollTo(0, 0);
   }
@@ -20,7 +22,13 @@ export default function Navbar(): ReactElement {
     <nav className="sm:px-16 px-6 w-full flex items-center py-5 fixed top-0 z-20 bg-primary">
       <div className="w-full flex justify-between items-center max-w-7xl mx-auto">
         <Link href="/" className="flex items-center gap-2" onClick={handleActive}>
-          {/* <Image src={logo} alt="logo" className="w-9 h-9 object-contain" /> */}
+          <Image
+            src={"/public/logo.svg"}
+            alt="logo"
+            className="w-9 h-9 object-contain"
+            width={0}
+            height={0}
+          />
           <p className="text-white text-[18px] font-bold cursor-pointer flex">
             <span className="sm:block hidden"> YJ &nbsp;| FullStack Developer</span>
           </p>
