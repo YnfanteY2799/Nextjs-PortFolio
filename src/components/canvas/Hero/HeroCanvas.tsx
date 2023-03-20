74703a03b54fdb2b74098bee0ff2518be682772a
Yassett J Ynfante Jimenez <49245097+YnfanteY2799@users.noreply.github.com>
2023-03-20 13:04:47 -0400

    +added mobile responsive for Hero

--- src/components/canvas/Hero/HeroCanvas.tsx
+++ src/components/canvas/Hero/HeroCanvas.tsx
@@ -1,11 +1,27 @@
-import { ReactElement, Suspense, useState } from "react";
+import { ReactElement, Suspense, useEffect, useState } from "react";
 import { Canvas } from "@react-three/fiber";
 import { CanvasLoader } from "@/components/canvas";
 import { OrbitControls, Preload } from "@react-three/drei";
 import HeroModel from "./HeroModel";
 
 export default function HeroCanvas(): ReactElement {
-  const [isMobile, setIsMobile] = useState();
+  const [isMobile, setIsMobile] = useState(false as boolean);
+
+  useEffect(() => {
+    const mq = window.matchMedia("(max-width: 500px)");
+
+    setIsMobile(mq.matches);
+
+    function handleMqChange(_event: MediaQueryListEvent): void {
+      setIsMobile(mq.matches);
+    }
+
+    mq.addEventListener("change", handleMqChange);
+
+    return () => {
+      mq.removeEventListener("change", handleMqChange);
+    };
+  }, []);
 
   return (
     <Canvas
--- src/components/page_componentials/index_page/Hero/Hero.tsx
+++ src/components/page_componentials/index_page/Hero/Hero.tsx
@@ -13,12 +13,9 @@ export default function Hero(): ReactElement {
           <h1 className="font-black text-white lg:text-[80px] sm:text-[60px] xs:text-[50px] text-[40px] lg:leading-[98px] mt-2">
             Hi, I'm <span className="text-orange-600"> Yassett </span>
           </h1>
-          <p
-            className={`mt-2 font-medium lg:text-[30px] sm:text-[26px] xs:text-[20px] text-[16px] lg:leading-[40px] text-white-100`}
-          >
-            I'm a FullStack
+          <p className="mt-2 font-medium lg:text-[30px] sm:text-[26px] xs:text-[20px] text-[16px] lg:leading-[40px] text-white-100">
+            I'm a FullStack Javascript Developer
             <br className="hidden sm:block" />
-            Javascript, web Developer
           </p>
         </div>
       </div>
