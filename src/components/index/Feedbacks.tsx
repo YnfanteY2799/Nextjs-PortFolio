072a0e3947878886d1fafca576b0bc4292b9da5a
Yassett J Ynfante Jimenez <49245097+YnfanteY2799@users.noreply.github.com>
2023-04-19 19:25:47 -0400

    +updates

--- src/components/index/Feedbacks.tsx
+++ src/components/index/Feedbacks.tsx
@@ -3,5 +3,5 @@ import { ReactElement, useState } from "react";
 export default function Hero(): ReactElement {
   const [text, setText] = useState("Ts" as string);
 
-  return <>Hey!{text}</>;
+  return <></>;
 }
--- src/pages/api/log/index.ts
+++ src/pages/api/log/index.ts
@@ -5,6 +5,9 @@ type Res = { response: string };
 export default function handler({ query }: NextApiRequest, res: NextApiResponse<Res>) {
   const { uname, pw } = query;
 
+  // console.log(uname, pw);
+  // console.log(process.env['HOST']);
+
   if (uname === undefined && pw === undefined) {
     res.status(404).json({ response: "error no data provided pls contact admin!" });
   } else {
